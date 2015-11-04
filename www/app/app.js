da65c70b85085ca8db96e1172c05ba11a89688ea
Cihan Duruer <cihan.duruer@gmail.com>
2015-11-04 17:29:50 +0100

    Stable version

--- www/app/app.js
+++ www/app/app.js
@@ -5,68 +5,102 @@
   angular.module("documentmanager", [
       'ui.router'
     ])
+    .constant("BASE_API_URL", "http://base.url")
     .config(function ($stateProvider) {
       $stateProvider
-        .state('index', {
-          url: "/",
+        .state("index", {
+          url: "",
           views: {
-            "menu": {
+            "menu@": {
               templateUrl: "app/main/menu.html"
             },
-            "left-pane-content": {
+            "left-pane-content@": {
               template: "Nothing in this place"
             },
-            "right-pane-content": {
+            "right-pane-content@": {
               template: "Nothing in this place"
             }
           }
         })
-        .state('documents', {
+        .state("documents", {
           url: "/documents",
           views: {
-            "menu": {
+            "menu@": {
               templateUrl: "app/document/views/menu.html"
             },
-            "left-pane-content": {
+            "left-pane-content@": {
               templateUrl: "app/document/views/list.html",
               controller: "DocumentController"
             },
-            "right-pane-content": {
-              template: "Nothing in this place"
+            "right-pane-content@": {
+              template: "Click 'Open' to see preview"
             }
           }
         })
-        .state("documents.browse", {
+        .state("documents.detail", {
           url: "/:documentid",
           views: {
-            "right-pane-content": {
+            "menu@": {
+              templateUrl: "app/main/menu.html"
+            },
+            "right-pane-content@": {
               templateUrl: "app/document/views/detail.html",
               controller: "DocumentDetailController"
             }
           }
         })
     })
-    .controller("MainController", function ($scope) {
-      $scope.stuff = 'stuff goes here';
-    })
-    .controller("DocumentController", function ($scope, $state) {
-      var state = $state;
-
-      $scope.documents = [{
-        "id": "1",
-        "name": "Document1"
-      }, {
-        "id": "2",
-        "name": "Document2"
-      }];
-
-      $scope.openDocument = function (id) {
-        state.go('documents.browse', {
-          documentid: id
-        });
+    .service('backendApiService', function BackendApiService() {
+      this.getDocuments = function () {
+        return [{
+          "id": "1",
+          "name": "Document1",
+          "image": "http://image.slidesharecdn.com/examplerequirementsspecification-120509024427-phpapp01/95/example-requirements-specification-1-728.jpg?cb=1336531501"
+        }, {
+          "id": "2",
+          "name": "Document2",
+          "image": "http://image.slidesharecdn.com/examplerequirementsspecification-120509024427-phpapp01/95/example-requirements-specification-2-728.jpg?cb=1336531501"
+        }];
       };
     })
-    .controller("DocumentDetailController", function ($scope, $stateParams) {
-      console.log("Selected documents : ", $stateParams["documentid"]);
+    .factory('backendApiFactory', function () {
+      return function () {
+        return [{
+          "id": "3",
+          "name": "Document3",
+          "image": "http://image.slidesharecdn.com/examplerequirementsspecification-120509024427-phpapp01/95/example-requirements-specification-3-728.jpg?cb=1336531501"
+        }, {
+          "id": "4",
+          "name": "Document4",
+          "image": "http://image.slidesharecdn.com/examplerequirementsspecification-120509024427-phpapp01/95/example-requirements-specification-4-728.jpg?cb=1336531501"
+        }];
+      }
+    })
+    .controller("MainController", function ($scope) {
+      console.log("MainController is created");
+      $scope.stuff = 'stuff goes here';
+    })
+    .controller("DocumentDetailController", function ($scope, $stateParams, backendApiService, backendApiFactory) {
+      console.log("DocumentDetailController is created");
+      var documents = backendApiService.getDocuments().concat(backendApiFactory());
+      var id = $stateParams["documentid"];
+      $scope.selectedDocumentImage = "";
+
+      for (var i = 0; i < documents.length; i++) {
+        if (documents[i].id === id) {
+          $scope.selectedDocumentImage = documents[i].image;
+        }
+      }
+
+    })
+    .controller("DocumentController", function ($scope, $state, backendApiService, backendApiFactory) {
+      console.log("DocumentController is created");
+      $scope.documents = backendApiService.getDocuments().concat(backendApiFactory());
+
+      $scope.openDocument = function (documentid) {
+        $state.go('documents.show', {
+          id: documentid
+        });
+      };
     });
 })();
